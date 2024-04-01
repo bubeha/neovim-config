2c3926fe21479f15ff4d466f626a91072867bde7
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-04-02 00:36:42 +0300

    feat: add ksp references remap

--- lua/bubeha/remap.lua
+++ lua/bubeha/remap.lua
@@ -3,3 +3,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jk", "<Esc>" )
+
+vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
