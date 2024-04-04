55cbc747d5a5abfbd85fdf23e4c00e4991906960
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-04-05 00:14:50 +0300

    feat: add better escape plugin

--- lua/bubeha/remap.lua
+++ lua/bubeha/remap.lua
@@ -2,6 +2,4 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("i", "jk", "<Esc>" )
-
 vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
