99aa82a660d1606ae8b6c19bc859ead2a6a75827
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-04-05 23:43:38 +0300

    feat: add <Tab> <S-Tab> keymap bindings

--- lua/bubeha/remap.lua
+++ lua/bubeha/remap.lua
@@ -3,3 +3,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("i", "<S-Tab>", "<Esc><<_i", { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
