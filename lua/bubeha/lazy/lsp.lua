8238bd64a7d74899de5573fa973470257eb0519f
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-04-28 22:19:38 +0300

    feat: add eslint to lsp

--- lua/bubeha/lazy/lsp.lua
+++ lua/bubeha/lazy/lsp.lua
@@ -1,120 +1,119 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        -- LSP Support
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		-- LSP Support
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 
-        -- Autocomplete
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
+		-- Autocomplete
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"saadparwaiz1/cmp_luasnip",
+		"L3MON4D3/LuaSnip",
 
-        "j-hui/fidget.nvim"
-    },
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
+		"j-hui/fidget.nvim",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
 
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "tsserver",
-                "gopls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"tsserver",
+				"gopls",
+				"eslint",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
 
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim", "it", "describe", "before_each", "after_each" },
+								},
+							},
+						},
+					})
+				end,
 
-                ["gopls"] = function()
-                    local lspconfig = require('lspconfig')
+				["gopls"] = function()
+					local lspconfig = require("lspconfig")
 
-                    lspconfig.gopls.setup {
-                        capabilities = capabilities,
-                        cmd = { "gopls", "serve" },
-                        filetypes = { "go", "gomod" },
-                        root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
-                        settings = {
-                            gopls = {
-                                completeUnimported = true,
-                                usePlaceholders = true,
-                                staticcheck = true,
-                                analyses = {
-                                    unusedparams = true,
-                                },
-                            },
-                        },
-                    }
-                end
-            }
-        })
+					lspconfig.gopls.setup({
+						capabilities = capabilities,
+						cmd = { "gopls", "serve" },
+						filetypes = { "go", "gomod" },
+						root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
+						settings = {
+							gopls = {
+								completeUnimported = true,
+								usePlaceholders = true,
+								staticcheck = true,
+								analyses = {
+									unusedparams = true,
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources(
-                {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' }, -- For luasnip users.
-                },
-                {
-                    { name = 'buffer' },
-                }
-            )
-        })
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end,
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
 }
