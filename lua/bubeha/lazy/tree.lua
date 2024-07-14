49b028d799aa1c3dd9e7445144516dc2e636e952
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-07-15 01:09:37 +0300

    chore: update tree.lua config

--- lua/bubeha/lazy/tree.lua
+++ lua/bubeha/lazy/tree.lua
@@ -1,19 +1,19 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        require("nvim-tree").setup {
-            filters = { custom = { "^.git$" } },
-            update_focused_file = { enable = true },
-        }
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			filters = { custom = { "^.git$" } },
+			update_focused_file = { enable = true },
+		})
 
-        vim.keymap.set("n", "<C-1>", require("nvim-tree.api").tree.toggle, {
-            silent = true,
-            desc = "toggle nvim-tree",
-        })
-    end,
+		vim.keymap.set("n", "<M-1>", require("nvim-tree.api").tree.toggle, {
+			silent = true,
+			desc = "toggle nvim-tree",
+		})
+	end,
 }
